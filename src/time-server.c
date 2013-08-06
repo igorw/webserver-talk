9d5c9e875afa6922533af9c0e23342a4db9f4d16
Igor Wiedler <igor@wiedler.ch>
2013-08-06 14:30:03 +0200

    Threaded version of time-server

--- src/time-server.c
+++ src/time-server.c
@@ -4,38 +4,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/types.h>
 #include <time.h>
+#include <pthread.h>
+#include "dbg.h"
+
+void *handle_conn(void *param)
+{
+    int *connfd = (int *) param;
+    time_t ticks;
+    char sendBuff[1025];
+
+    ticks = time(NULL);
+
+    check(snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks)) > 0, "snprintf");
+    check(write(*connfd, sendBuff, strlen(sendBuff)) != -1, "write");
+
+    sleep(20);
+
+    close(*connfd);
+    free(param);
+
+    return NULL;
+
+error:
+    if (connfd && *connfd)
+        close(*connfd);
+    if (param)
+        free(param);
+    return NULL;
+}
 
 int main(int argc, char *argv[])
 {
-    int listenfd = 0, connfd = 0;
+    int listenfd = 0;
     struct sockaddr_in serv_addr;
 
-    char sendBuff[1025];
-    time_t ticks;
+    pthread_t             thread;
+    int                   rc=0;
+    void                 *status;
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    memset(sendBuff, '0', sizeof(sendBuff));
+    check(listenfd != -1, "create socket");
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(5000);
 
-    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-    listen(listenfd, 10);
+    check(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) != -1, "bind");
+    check(listen(listenfd, 10) != -1, "listen");
+
+    printf("waiting...\n");
 
     while(1)
     {
-        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+        int *connfd = malloc(sizeof(int));
+        check_mem(connfd);
 
-        ticks = time(NULL);
-        snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
-        write(connfd, sendBuff, strlen(sendBuff));
+        *connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+        check(*connfd != -1, "accept");
+        rc = pthread_create(&thread, NULL, handle_conn, (void *) connfd);
+        check(rc == 0, "pthread_create");
 
-        close(connfd);
-     }
+        printf("accepted\n");
+    }
+
+error:
+    return 1;
 }
